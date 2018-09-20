0e7849f158280ac41afd98ad3f59a6fc5ea1cc38
wujiaming <wujiaming@iplas.com.cn>
2018-09-20 13:54:27 +0800

    push

---
+++ canal/src/main/java/com/canal/KafkaHandler.java
@@ -0,0 +1,25 @@
+package com.canal;
+
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class KafkaHandler implements BinlogHandler {
+    @Override
+    public void process(List<CanalEntry.Entry> entries) {
+        entries.stream().filter((entry) -> filter(entry)).forEach((entry) -> {
+            try {
+                CanalEntry.RowChange row = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
+            } catch (InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private boolean filter(CanalEntry.Entry entry) {
+        List<CanalEntry.EntryType> filters = Arrays.asList(CanalEntry.EntryType.TRANSACTIONBEGIN, CanalEntry.EntryType.TRANSACTIONEND);
+        return !filters.contains(entry.getEntryType());
+    }
+}
